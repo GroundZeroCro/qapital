247aef4d4db996b725cd8f016896a4b2997702f7
mario dujic <mario.dujic@gmail.com>
2019-08-22 09:17:40 +0200

    restructured ViewModelFactory function from if to when

--- app/src/main/java/com/groundzero/qapital/ui/common/ViewModelFactory.kt
+++ app/src/main/java/com/groundzero/qapital/ui/common/ViewModelFactory.kt
@@ -8,18 +8,17 @@ import com.groundzero.qapital.ui.details.DetailsViewModel
 import com.groundzero.qapital.ui.goal.GoalViewModel
 import javax.inject.Inject
 
+@Suppress("UNCHECKED_CAST")
 class ViewModelFactory @Inject constructor(
     private val goalRepository: GoalRepository,
     private val detailsRepository: DetailsRepository
 ) : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return if (modelClass.isAssignableFrom(GoalViewModel::class.java)) {
-            GoalViewModel(this.goalRepository) as T
-        } else if (modelClass.isAssignableFrom(DetailsViewModel::class.java)) {
-            DetailsViewModel(this.detailsRepository) as T
-        } else {
-            throw IllegalArgumentException("ViewModel Not Found")
+        return when {
+            modelClass.isAssignableFrom(GoalViewModel::class.java) -> GoalViewModel(this.goalRepository) as T
+            modelClass.isAssignableFrom(DetailsViewModel::class.java) -> DetailsViewModel(this.detailsRepository) as T
+            else -> throw IllegalArgumentException("ViewModel Not Found")
         }
     }
 
